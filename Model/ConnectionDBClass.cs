eeaa2ff1139932117d0bd6f40b00d9bc07fbc0bd
Andi Azamuddin bin Andianwar <andiazamuddin@gmail.com>
2021-06-24 08:43:37 +0800

    Add files via upload

---
+++ Model/ConnectionDBClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RetrievingData.Model
+{
+    public class ConnectionDBClass:DbContext
+    {
+        public ConnectionDBClass(DbContextOptions<ConnectionDBClass> options):base(options)
+        {
+
+        }
+        public DbSet<EmpClass> DimEmployee { get; set; }
+    }
+}
---
+++ Model/EmpClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RetrievingData.Model
+{
+    public class EmpClass
+    {
+        [Key]
+        public int EmployeeKey { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
